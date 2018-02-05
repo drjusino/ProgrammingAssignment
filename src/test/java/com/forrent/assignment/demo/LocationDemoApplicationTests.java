1646dc717f41a73ed03007e90429af22cf69e49c
David Jusino <davidraymondjusino@gmail.com>
2018-02-05 11:36:30 -0500

    Initial Commit

---
+++ src/test/java/com/forrent/assignment/demo/LocationDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.forrent.assignment.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LocationDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
