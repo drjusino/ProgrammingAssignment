ee213b5bccff89a9fad850cb6b9bacdcadeb4f60
David Jusino <davidraymondjusino@gmail.com>
2018-02-05 11:16:27 -0500

    Initial Commit

---
+++ src/main/java/com/forrent/assignment/demo/ParkLocatorDemoApplication.java
@@ -0,0 +1,12 @@
+package com.forrent.assignment.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkLocatorDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkLocatorDemoApplication.class, args);
+    }
+}
